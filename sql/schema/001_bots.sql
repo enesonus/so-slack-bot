3e33733ee4470636701bdbd43a38eea2e91ebd71
enesonus <enes.onus@getir.com>
2023-10-17 11:57:18 +0300

    not null added

--- sql/schema/001_bots.sql
+++ sql/schema/001_bots.sql
@@ -4,7 +4,7 @@ CREATE TABLE bots (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     last_activity_at TIMESTAMP NOT NULL DEFAULT NOW(),
     bot_token VARCHAR(255) NOT NULL,
-    workspace_id TEXT REFERENCES workspaces(id) ON DELETE CASCADE
+    workspace_id TEXT NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE
 );
 
 -- +goose Down
--- sql/schema/003_channels.sql
+++ sql/schema/003_channels.sql
@@ -3,7 +3,7 @@ CREATE TABLE channels (
     id TEXT UNIQUE PRIMARY KEY,
     channel_name TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    workspace_id TEXT REFERENCES workspaces(id) ON DELETE CASCADE
+    workspace_id TEXT NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE
 );
 
 -- +goose Down
