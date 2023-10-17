4c0b3e58fd590f2fdd5ff564bcb2a53b8e7ecc9f
enesonus <enes.onus@getir.com>
2023-10-17 11:57:43 +0300

    added getorcreateworkspace

--- sql/queries/workspaces.sql
+++ sql/queries/workspaces.sql
@@ -2,3 +2,17 @@
 INSERT INTO workspaces (id, workspace_name, workspace_domain, created_at)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+-- name: GetOrCreateWorkspace :one
+INSERT INTO workspaces (id, workspace_name, workspace_domain, created_at)
+VALUES ($1, $2, $3, $4)
+ON CONFLICT (id)
+DO UPDATE SET 
+    workspace_name = EXCLUDED.workspace_name, 
+    workspace_domain = EXCLUDED.workspace_domain, 
+    created_at = EXCLUDED.created_at
+WHERE 
+    workspaces.workspace_name != EXCLUDED.workspace_name OR 
+    workspaces.workspace_domain != EXCLUDED.workspace_domain OR 
+    workspaces.created_at != EXCLUDED.created_at
+RETURNING *;
