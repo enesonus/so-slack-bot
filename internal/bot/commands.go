23ab1f9d7816541518a76bc025258185a3c4c6e8
enesonus <enes.onus@getir.com>
2023-10-19 17:51:42 +0300

    refactor startSlackBot behavior for errors and add db_helpers

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -11,34 +11,23 @@ import (
 	"github.com/shomali11/slacker"
 )
 
-func StartSlackBot(slackBotToken string) error {
+func StartSlackBot(slackBotToken string) (*slacker.Slacker, error) {
 
-	fmt.Println("Slack Bot Token: ", slackBotToken)
 	slackBot := slacker.NewClient(slackBotToken, os.Getenv("SLACK_APP_TOKEN"))
-
 	slackBot.Command("set_so_channel", setSOChannelDef)
 	slackBot.Command("remove_so_channel", removeSOChannelDef)
 	slackBot.Command("getinfo", getUserInfoDef)
+	slackBot.Command("show_tags", showTagsDef)
 	slackBot.Command("add_tag {tag}", addTagDef)
-
-	ctx, cancel := context.WithCancel(context.Background())
 
 	go PrintCommandEvents(slackBot.CommandEvents())
 
-	err := error(nil)
-	go func() {
-		err = slackBot.Listen(ctx)
-		defer cancel()
-	}()
-	if err != nil {
-		log.Printf("Error listening to Slack Bot: %v, Token: %v\n", err, slackBotToken)
-		return fmt.Errorf("error listening to slack bot: %v, token: %v", err, slackBotToken)
-	}
+	// Add bot and Workspace to DB
 
 	teamInfo, err := slackBot.APIClient().GetTeamInfo()
 	if err != nil {
 		log.Printf("Error getting team info: %v\n", err)
-		return fmt.Errorf("error getting team info: %v", err)
+		return nil, fmt.Errorf("error getting team info: %v", err)
 	}
 
 	workspaceParams := db.GetOrCreateWorkspaceParams{
@@ -55,8 +44,8 @@ func StartSlackBot(slackBotToken string) error {
 
 	_, err = databaseObject.GetOrCreateWorkspace(context.Background(), workspaceParams)
 	if err != nil {
-		log.Printf("Error creating workspace: %v\n", err)
-		return fmt.Errorf("error creating workspace: %v", err)
+		log.Printf("Error adding workspace to DB: %v\n", err)
+		return nil, fmt.Errorf("error adding workspace to DB: %v", err)
 	}
 	botParams := db.CreateBotParams{
 		BotToken:       slackBotToken,
@@ -66,9 +55,20 @@ func StartSlackBot(slackBotToken string) error {
 	}
 	_, err = databaseObject.CreateBot(context.Background(), botParams)
 	if err != nil {
-		log.Printf("Error creating bot: %v\n", err)
-		return fmt.Errorf("error creating bot: %v", err)
+		log.Printf("Error adding bot to DB: %v\n", err)
+		return slackBot, fmt.Errorf("error adding bot to DB: %v", err)
 	}
 
-	return nil
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		err = slackBot.Listen(ctx)
+		defer cancel()
+	}()
+
+	if err != nil {
+		log.Printf("Error listening to Slack Bot: %v, Token: %v\n", err, slackBotToken)
+		return nil, fmt.Errorf("error listening to slack bot: %v, token: %v", err, slackBotToken)
+	}
+
+	return slackBot, nil
 }
--- internal/db/db_helpers.go
+++ internal/db/db_helpers.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -26,4 +28,33 @@ func GetDatabase() (*Queries, error) {
 	database := New(conn)
 
 	return database, err
+}
+
+func TestDatabase(database *Queries, count int) {
+
+	start := time.Now()
+	for i := 0; i < count; i++ {
+
+		params := CreateBotParams{
+			CreatedAt:      time.Now(),
+			LastActivityAt: time.Now(),
+			BotToken:       "firstBotToken",
+		}
+
+		_, err := database.CreateBot(context.Background(), params)
+		if err != nil {
+			log.Fatal("Couldn't create bot: ", err)
+		}
+	}
+	fmt.Printf("Time to create %v bots: %v\n", count, time.Since(start))
+
+	start = time.Now()
+	bots, err := database.GetBots(context.Background())
+	fmt.Printf("Time to get bots: %v\n", time.Since(start))
+
+	if err != nil {
+		log.Fatal("Couldn't get bots: ", err)
+	}
+	// fmt.Println("Bot just created: ", botFromDB)
+	fmt.Printf("Total Bot count: %v\n", len(bots))
 }
