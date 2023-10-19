6ed3f858566662d99aaf75c476e73d1a0c402663
enesonus <enes.onus@getir.com>
2023-10-19 18:44:44 +0300

    add subscriber methods

--- sql/queries/tag_subscriptions.sql
+++ sql/queries/tag_subscriptions.sql
@@ -2,3 +2,28 @@
 INSERT INTO tag_subscriptions (tag, channel_id)
 VALUES ($1, $2)
 RETURNING *;
+
+-- name: UnbindTag :one
+DELETE FROM tag_subscriptions
+WHERE tag = $1 AND channel_id = $2
+RETURNING *;
+
+-- name: GetTagSubscriptions :one
+SELECT * FROM tag_subscriptions;
+
+-- name: GetTagSubscriptionsWithName :many
+SELECT * FROM tag_subscriptions WHERE tag = $1;
+
+-- name: GetTagSubscriptionsWithChannelId :many
+SELECT * FROM tag_subscriptions WHERE channel_id = $1;
+
+-- name: GetSubscriberChannels :one
+SELECT 
+    channels.*
+FROM 
+    channels
+JOIN 
+    tag_subscriptions 
+    ON channels.id = tag_subscriptions.channel_id
+WHERE 
+    tag_subscriptions.tag = $1;
