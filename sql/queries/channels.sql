ebe2ae6718b53b56ce532fff754925661821d68b
enesonus <enes.onus@getir.com>
2023-10-20 23:05:25 +0300

    add GetChannelByBotToken to db ops

--- sql/queries/channels.sql
+++ sql/queries/channels.sql
@@ -1,8 +1,11 @@
 -- name: CreateChannel :one
-INSERT INTO channels (id, channel_name, created_at, workspace_id)
-VALUES ($1, $2, $3, $4)
+INSERT INTO channels (id, channel_name, created_at, workspace_id, bot_token)
+VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
 -- name: DeleteChannel :one
 DELETE FROM channels WHERE id = $1
 RETURNING *;
+
+-- name: GetChannelByBotToken :many
+SELECT * FROM channels WHERE bot_token = $1;
