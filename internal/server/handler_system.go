f3498f9a24a1298c2db3676d09d9cb96dbc3f1f9
enesonus <enes.onus@getir.com>
2023-10-23 21:18:11 +0300

    add localhost check for debugging purposes

--- internal/server/handler_system.go
+++ internal/server/handler_system.go
@@ -2,8 +2,15 @@ package server
 
 import (
 	"net/http"
+	"os"
+	"strings"
 )
 
 func CheckReadiness(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, map[string]string{"ready": "OK"})
+	message := "Server"
+	hostname, _ := os.Hostname()
+	if strings.HasSuffix(hostname, "local") {
+		message = "Localhost"
+	}
+	respondWithJSON(w, 200, map[string]string{"ready": message})
 }
