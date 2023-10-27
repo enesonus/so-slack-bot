b61209656ac1d76b09356400c7beca4793de4caa
enesonus <enes.onus@getir.com>
2023-10-27 22:45:02 +0300

    Add SlackMessageContext, SlackMessageHandler, CommandFunc, NewClient etc.

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/enesonus/so-slack-bot/internal/db"
 	"github.com/shomali11/slacker"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 func PrintCommandEvents(slackChannel <-chan *slacker.CommandEvent) {
@@ -27,7 +31,7 @@ func CreateSlackBot(slackBotToken string) (*slacker.Slacker, error) {
 	slackBot.Command("remove_so_channel", removeSOChannelDef)
 	slackBot.Command("getinfo", getUserInfoDef)
 	slackBot.Command("add_tag {tag}", addTagDef)
-	slackBot.Command("show_tags", showTagsDef)
+	// slackBot.Command("show_tags", showTagsDef)
 
 	go PrintCommandEvents(slackBot.CommandEvents())
 
@@ -88,7 +92,7 @@ func StartSlackBot(slackBotToken string) (*slacker.Slacker, error) {
 	slackBot.Command("remove_so_channel", removeSOChannelDef)
 	slackBot.Command("getinfo", getUserInfoDef)
 	slackBot.Command("add_tag {tag}", addTagDef)
-	slackBot.Command("show_tags", showTagsDef)
+	// slackBot.Command("show_tags", showTagsDef)
 
 	go PrintCommandEvents(slackBot.CommandEvents())
 
@@ -103,4 +107,94 @@ func StartSlackBot(slackBotToken string) (*slacker.Slacker, error) {
 	}
 
 	return slackBot, nil
+}
+
+func SlackMessageHandler(w http.ResponseWriter, eventsAPIEvent slackevents.EventsAPIEvent) (*SlackMessageContext, error) {
+	start := time.Now()
+	innerEvent := eventsAPIEvent.InnerEvent
+	ev := innerEvent.Data.(*slackevents.MessageEvent)
+	if ev.BotID != "" {
+		w.WriteHeader(http.StatusOK)
+		return &SlackMessageContext{}, nil
+	}
+	msgCtx, err := NewClient(eventsAPIEvent)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return &SlackMessageContext{}, fmt.Errorf("error at NewClient: %v", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Printf("SlackMessageHandler took %v\n", time.Since(start))
+	return msgCtx, nil
+}
+
+func getSlackAPIClient(workspaceID string) (*slack.Client, error) {
+
+	dbObj, err := db.GetDatabase()
+	if err != nil {
+		return nil, fmt.Errorf("error at GetDatabase: %v", err)
+	}
+	botObj, err := dbObj.GetBotByWorkspaceID(context.Background(), workspaceID)
+	if err != nil {
+		return nil, fmt.Errorf("error at GetBotByWorkspaceID: %v", err)
+	}
+	api := slack.New(botObj.BotToken)
+	return api, nil
+}
+
+func (messageCtx *SlackMessageContext) ChannelName() (string, error) {
+	channel, err := messageCtx.api.GetConversationInfo(&slack.GetConversationInfoInput{
+		ChannelID:         messageCtx.channelID,
+		IncludeLocale:     false,
+		IncludeNumMembers: false})
+	if err != nil {
+		return "", fmt.Errorf("error getting channel: %v", err)
+	}
+	return channel.Name, nil
+}
+
+type CommandFunc func(msgCtx *SlackMessageContext, suffix string)
+type SlackMessageContext struct {
+	api            *slack.Client
+	eventsAPIEvent slackevents.EventsAPIEvent
+	innerEvent     *slackevents.MessageEvent
+	message        string
+	prefix         string
+	channelID      string
+}
+
+func NewClient(eventsAPIEvent slackevents.EventsAPIEvent) (*SlackMessageContext, error) {
+
+	innerEvent := eventsAPIEvent.InnerEvent
+	if innerEvent.Type != "message" {
+		return &SlackMessageContext{}, fmt.Errorf("error at NewClient: innerEvent.Type is not MessageEvent")
+	}
+	ev := innerEvent.Data.(*slackevents.MessageEvent)
+	message := ev.Text
+	words := strings.Fields(message)
+	prefix := ""
+	if len(words) > 0 {
+		prefix = words[0]
+	}
+	api, err := getSlackAPIClient(eventsAPIEvent.TeamID)
+	if err != nil {
+		return &SlackMessageContext{}, fmt.Errorf("error at NewClient/getSlackAPIClient: %v", err)
+	}
+	return &SlackMessageContext{
+		api:            api,
+		eventsAPIEvent: eventsAPIEvent,
+		innerEvent:     ev,
+		message:        message,
+		prefix:         prefix,
+		channelID:      ev.Channel,
+	}, nil
+}
+
+func (messageCtx *SlackMessageContext) Listen(commandPrefix string, commandFunc CommandFunc) {
+	suffix := ""
+	if len(messageCtx.message) > len(commandPrefix) {
+		suffix = messageCtx.message[len(commandPrefix):]
+	}
+	if messageCtx.prefix == commandPrefix {
+		commandFunc(messageCtx, suffix)
+	}
 }
